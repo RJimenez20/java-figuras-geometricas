b574fd1adf9ec5785fc0b714dc801bc5c600553b
Ricardo Jimenez <ricardojimenez.dev@gmail.com>
2026-05-26 22:20:42 -0500

    Creando clase triangulo rectangulo

---
+++ TrianguloRectangulo.java
@@ -0,0 +1,27 @@
+public class TrianguloRectangulo {
+    int base;
+    int altura;
+    public TrianguloRectangulo(int base, int altura){
+        this.base = base;
+        this.altura = altura;
+    }
+
+    double areaTrianguloRectangulo(){
+        return ((base*altura)/2);
+    }
+    double perimetroTrianguloRectangulo(){
+        return base + altura + calculoHipotenusa();
+    }
+
+    double calculoHipotenusa(){
+        return Math.pow(base*base+altura*altura,0.5);
+    }
+    void determinarTipoTriángulo() {
+        if ((base == altura) && (base == calculoHipotenusa()) && (altura == calculoHipotenusa()))
+            System.out.println("Es un triángulo equilátero");
+        else if ((base != altura) && (base != calculoHipotenusa()) && (altura != calculoHipotenusa()))
+            System.out.println("Es un triángulo escaleno");
+        else
+            System.out.println("Es un triángulo isósceles");
+    }
+}
